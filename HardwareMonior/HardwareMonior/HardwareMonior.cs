8214fc04b5d8e765beee24c0d13e08bddd4a9454
jungjaekyun <jungjaekyun@ubisam.com>
2024-07-03 17:04:36 +0900

    init

---
+++ HardwareMonior/HardwareMonior/HardwareMonior.cs
@@ -0,0 +1,30 @@
+﻿using System.Net.NetworkInformation;
+using System;
+using System.Threading;
+using System.Collections.Generic;
+
+namespace HardwareMonior
+{
+    public struct HardwareMonioringData
+    {
+        float _process_total;
+        List<float> _process_child;
+
+    }
+
+
+    static public class HardwareMonior
+    {
+
+        static public void Init()
+        {
+
+        }
+        
+        static public void Update()
+        {
+
+        }
+
+    }
+}
